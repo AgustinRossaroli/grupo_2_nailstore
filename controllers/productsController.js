f82e40446fbe8b165f3f0489d0137974e6fca9b2
AgustinRossaroli <agustinrossaroli@gmail.com>
2023-02-20 18:04:36 -0300

    merge

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -40,88 +40,6 @@ const productsController = {
             price,
             image
         } = req.body;
-    postProductCreator: (req, res) => {
-        const {
-            name,
-            description,
-            category,
-            price,
-            image
-        } = req.body;
-
-        const newId = products[products.length - 1].id + 1;
-        const newId = products[products.length - 1].id + 1;
-
-        const obj = {
-            id: newId,
-            name,
-            description,
-            category,
-            price,
-            image
-        }
-        products.push(obj)
-        res.redirect("/shop")
-    },
-    filename: path.join(__dirname, "../data/products1.json"),
-
-    getAllProducts: () => {
-        return JSON.parse(fs.readFileSync(productsController.filename, "utf-8"));
-    },
-    editarProducto: (req, res) => {
-        const userSession = req.cookies.userSession;
-
-        if (userSession) {
-            const { id } = req.params;
-            let allProducts = productsController.getAllProducts();
-
-            const editarProducto = allProducts.find(i => i.id == id);
-
-            res.render(path.resolve(__dirname, "../views/products/editarProducto.ejs"), { editarProducto });
-        } else {
-            res.redirect("/iniciarSesion");
-        }
-    },
-    confirmarEdicion: (req, res) => {
-        let allProducts = productsController.getAllProducts();
-
-        const productoEditado = allProducts.map(i => {
-            if (i.id == req.body.id) {
-                i.image = req.body.imagen;
-                i.name = req.body.nombre;
-                i.category = req.body.categoria;
-                i.description = req.body.descripcion;
-                i.price = req.body.precio;
-            };
-            return i;
-        });
-        fs.writeFileSync(productsController.filename, JSON.stringify(productoEditado));
-        res.redirect("detalle/" + req.body.id);
-    },
-    detalle: (req, res) => {
-        const userSession = req.cookies.userSession;
-
-        if (userSession) {
-            const { id } = req.params;
-            let allProducts = productsController.getAllProducts();
-
-            const detalle = allProducts.find(i => i.id == id);
-
-            res.render(path.resolve(__dirname, "../views/products/detalleProducto.ejs"), { detalle });
-        } else {
-            res.redirect("/iniciarSesion");
-        };
-    }
-};
-
-const postProductCreator = (req, res) =>{
-    const {
-        name,
-        description,
-        category,
-        price,
-        image
-    } = req.body;
 
         const newId = products[products.length - 1].id + 1;
 
@@ -186,6 +104,7 @@ const postProductCreator = (req, res) =>{
         };
     }
 };
+
 module.exports = {
     productsController
 };
