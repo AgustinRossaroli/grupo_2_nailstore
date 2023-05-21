2ecee8f33372db732acbfd7c0099714ff7159503
AgustinRossaroli <agustinrossaroli@gmail.com>
2023-05-21 01:19:34 -0300

    termino endpoints api y responsives ejs

--- API/app.js
+++ API/app.js
@@ -13,6 +13,6 @@ app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(morgan("dev"))
-app.use("/api/users", usersRouter)
 
+app.use("/api/users", usersRouter)
 app.use("/api",productsRouter)
