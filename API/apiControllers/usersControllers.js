814a496b9cb6729337c78defffb466dd63142d6c
AgustinRossaroli <agustinrossaroli@gmail.com>
2023-05-22 22:06:10 -0300

    react

--- API/apiControllers/usersControllers.js
+++ API/apiControllers/usersControllers.js
@@ -3,25 +3,26 @@ const Op = DB.Sequelize.Op;
 
 module.exports = {
     list: (req, res) =>
-        DB.Users
-            .findAll({
-                attributes: ['name', 'email', "id"] // Especificar las propiedades que deseas retornar
-            })
-            .then(users => {
-                users = users.map((e) => {
-                    return {
-                        ...e.dataValues,
-                        id: e.id,
-                        name: e.name, // Solo agregar las propiedades "name" y "email"
-                        email: e.email,
-                        detail: "http://localhost:3000/api/users/" + e.id,
-                    };
-                });
-                return res.json({
-                    totalUsers: users.length,
-                    users: users
-                });
-            }),
+    DB.Users
+        .findAll({
+            attributes: ['name', 'email', 'id', 'image'] // Especificar las propiedades que deseas retornar
+        })
+        .then(users => {
+            users = users.map((e) => {
+                return {
+                    ...e.dataValues,
+                    id: e.id,
+                    name: e.name,
+                    email: e.email,
+                    image: `http://localhost:3000/api/users/${e.id}/image`,
+                    detail: `http://localhost:3000/api/users/${e.id}`,
+                };
+            });
+            return res.json({
+                totalUsers: users.length,
+                users: users
+            });
+        }),
     userById: (req, res) => {
         const { id } = req.params;
         DB.Users.findByPk(id, {
--- API/apiRoutes/usersRoutes.js
+++ API/apiRoutes/usersRoutes.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router()
-const { list, userById } = require("../apiControllers/usersControllers")
+const { list, userById, imageUser } = require("../apiControllers/usersControllers")
+const db = require("../database/models");
+const fs = require('fs');
+const path = require('path');
 
 router.get("/", list)
 router.get("/:id", userById)
-
+router.get('/:id/image', (req, res) => {
+    const userId = req.params.id;
+  
+    // Aquí debes implementar la lógica para obtener la imagen del usuario con el ID proporcionado
+    db.Users.findByPk(userId)
+      .then(user => {
+          console.log(user.image)
+        if (user && user.image) {
+            const imagePath = path.join(__dirname, '../../public/images', user.image);
+  
+          // Verificar si el archivo de imagen existe
+          if (fs.existsSync(imagePath)) {
+            // Enviar la imagen al cliente
+            res.sendFile(imagePath);
+          } else {
+            // Si la imagen no existe, puedes enviar una imagen predeterminada o una respuesta de error
+            res.status(404).send('Imagen no encontrada');
+          }
+        } else {
+          res.status(404).send('Usuario no encontrado');
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener la imagen del usuario:', error);
+        res.status(500).send('Error interno del servidor');
+      });
+  });
+  
 module.exports = router
